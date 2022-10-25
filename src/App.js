e9e0b91d58f21ce096ace35bf7086fceb0db4eb6
slavinto <slava3669@gmail.com>
2022-10-25 09:38:19 +0300

    Lesson77 Adding scss styling working with App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,47 @@
+import "./categories.style.scss";
+
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      categoryTitle: "Hats",
+      imgSrc: "",
+    },
+    {
+      id: 2,
+      categoryTitle: "Jackets",
+      imgSrc: "",
+    },
+    {
+      id: 3,
+      categoryTitle: "Sneakers",
+      imgSrc: "",
+    },
+    {
+      id: 4,
+      categoryTitle: "Womens",
+      imgSrc: "",
+    },
+    {
+      id: 5,
+      categoryTitle: "Mens",
+      imgSrc: "",
+    },
+  ];
+
   return (
-    <div className='categories-container'>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
+    <div className="categories-container">
+      {categories.map(({ id, categoryTitle, imgSrc }) => {
+        return (
+          <div key={id} className="category-container">
+            <div className="background-image" src={imgSrc} />
+            <div className="category-body-container">
+              <h2>{categoryTitle}</h2>
+              <p>Shop Now</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
